9709649e799c551e4a88198fc7bc21b5b40a5ece
Dmitrii Rachkov <Dmitrii_Rachkov@epam.com>
2023-03-31 20:38:28 +0300

    Unicode resolve 2

--- utils/checking.py
+++ utils/checking.py
@@ -9,7 +9,7 @@ class Checking():
     @staticmethod
     def check_status_code(status_code, result):
         assert status_code == result.status_code
-        print(f'Запрос прошёл успешно! Статус код: {result.status_code}')
+        print(f'Запрос прошёл успешно! Статус код: {result.status_code}'.encode('utf-8'))
 
 
     # Метод для проверки наличия обязательных полей
@@ -18,7 +18,7 @@ class Checking():
         # Функция 'loads' модуля 'json' преобразует получаемую строку в json формат
         actual_fields = json.loads(result.text)
         assert list(actual_fields) == expected_fields
-        print(f'Все обязательные поля присутствуют.')
+        print(f'Все обязательные поля присутствуют.'.encode('utf-8'))
 
 
     # Метод для проверки значений полей
@@ -27,7 +27,7 @@ class Checking():
         check = result.json()
         check_info = check.get(field_name)
         assert check_info == expected_value
-        print(f'Значение поля {field_name} верно.')
+        print(f'Значение поля {field_name} верно.'.encode('utf-8'))
 
 
 
