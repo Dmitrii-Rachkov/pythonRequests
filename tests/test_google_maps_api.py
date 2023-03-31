668607e2bcb62e659697d265a15e76e57612c954
Dmitrii Rachkov <Dmitrii_Rachkov@epam.com>
2023-03-31 21:41:49 +0300

    Sys unicode resolve 2

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,7 +1,3 @@
-import sys
-import io
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 from utils.api import Google_maps_api
 
 """Создание, изменение, удаление локации в google maps api"""
